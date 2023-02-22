41f0eba98887665017b502ac35d888d12114119b
Dmitry Elfimov <skitehok@mail.ru>
2023-02-22 23:25:14 +0500

    preved medved

---
+++ example006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "skit")
+{
+    Console.WriteLine("Hi, skit");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
