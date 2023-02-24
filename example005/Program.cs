5600e817debccabb800bf76fb7bf1b4d390d254c
DmitriyElfimov <125506026+DmitriyElfimov@users.noreply.github.com>
2023-02-24 16:36:20 +0500

    Delete example005 directory

--- example005/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int numberA = new Random().Next(0, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(0, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
