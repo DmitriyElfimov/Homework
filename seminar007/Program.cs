85a5b7f34b5e5909628812c838f5236fdd6b4e71
Dmitry Elfimov <skitehok@mail.ru>
2023-03-18 00:11:07 +0500

    homework07

---
+++ seminar007/Program.cs
@@ -0,0 +1,189 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
+double[,] FillMatrixWithRandom(int row, int column)
+{
+    double[,] matrix = new double[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble()*10;
+                    }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i,j], 2)} \t");  
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+*/
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// поиск числа
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string search = "Искомое число не найдено";
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i,j] == number)
+        {
+            search = $"Искомое число найдено и имеет координаты [{i}, {j}]";
+            break;
+        }
+    }
+}
+
+Console.WriteLine(search);
+*/
+
+// поиск элементов
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+Console.WriteLine("Введите строку элемента: ");
+int numberI = Convert.ToInt32(Console.ReadLine()); // Или здесь нужно задать значение на 1 меньше?? Чтобы пользователь ввел строку от 1 до.., а не индекс
+Console.WriteLine("Введите столбец элемента: ");
+int numberJ = Convert.ToInt32(Console.ReadLine());
+
+if(numberI <= row && numberJ <= column)
+{
+    Console.WriteLine($"Значение элемента на позиции [{numberI}, {numberJ}] = {matrix[numberI,numberJ]}");
+}
+else
+{
+    Console.WriteLine($"Элемента на позиции [{numberI}, {numberJ}] не существует.");
+}
+*/
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+double[] array = new double[column];
+for (int j = 0; j < column; j++)
+{
+    double summa = 0;
+    for (int i = 0; i < row; i++)
+    summa = summa + matrix[i, j];
+    array[j] = summa / row;
+    summa = 0;
+Console.Write($"{Math.Round(array[j], 2)} \t");    
+}
+
