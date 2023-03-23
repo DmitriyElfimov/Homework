bd98a5d29d5f11e498c7b88964d258cdc9aab4a0
Dmitry Elfimov <skitehok@mail.ru>
2023-03-23 20:02:31 +0500

    additional

--- seminar008/Program.cs
+++ seminar008/Program.cs
@@ -163,4 +163,182 @@ PrintMatrix(matrix);
 */
 
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
 
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк первой матрицы: ");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов первой матрицы: ");
+int column1 = Convert.ToInt32(Console.ReadLine());
+int[,] firstMatrix = FillMatrixWithRandom(row1, column1);
+Console.WriteLine("Введите кол-во строк второй матрицы: ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов второй матрицы: ");
+int column2 = Convert.ToInt32(Console.ReadLine());
+int[,] secondMatrix = FillMatrixWithRandom(row2, column2);
+PrintMatrix(firstMatrix);
+Console.WriteLine();
+PrintMatrix(secondMatrix);
+if (column1 == row2)
+{
+int [,] newMatrix = new int [row1, column2];
+for (int i = 0; i < row1; i++)
+    {
+        for (int j = 0; j < column2; j++)
+        {
+            for (int k = 0; k < column1; k++)
+            {
+                newMatrix[i,j] = newMatrix[i,j]+firstMatrix[i,k]*secondMatrix[k,j];
+            } 
+        }
+        
+    }
+Console.WriteLine();
+Console.WriteLine("Результат умножения:");    
+PrintMatrix(newMatrix);
+}
+else Console.WriteLine("такие матрицы перемножать нельзя");
+*/
+
+// Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+// на пересечении которых расположен наименьший элемент массива.
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+int min = matrix[0, 0];
+int m = 0;
+int n = 0;
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        if (matrix[i, j] < min)
+        {
+            min = matrix[i, j];
+            m = i;
+            n = j;
+        }
+
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Минимальный элемент {min} - [{m}, {n}]");
+
+
+int[,] newMatrix = new int[row - 1, column - 1];
+int i1 = 0;
+int j1 = 0;
+for (int i = 0; i < row - 1; i++)
+{
+    for (int j = 0; j < column - 1; j++)
+    {
+        if (i >= m) i1 = i + 1;
+        else i1 = i;
+        if (j >= n) j1 = j + 1;
+        else j1 = j;
+        newMatrix[i, j] = matrix[i1, j1];
+    }
+}
+Console.WriteLine();
+
+PrintMatrix(newMatrix);
+*/
+
+// Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+/*
+Console.WriteLine("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+int [,] triangle = new int [row,row];
+const int cellWidth = 3;
+
+void FillTriangle()
+{
+    for(int i = 0; i < row; i++)
+    {
+        triangle[i,0] = 1;
+        triangle[i,i] = 1;
+    }
+    for(int i = 2; i < row; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            triangle[i,j] = triangle[i-1, j-1] + triangle[i-1,j];
+        }
+    }
+}
+
+void Magic()
+{
+    int col = cellWidth * row;
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j<=i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if (triangle[i,j] != 0) Console.Write($"{triangle[i,j], cellWidth}");
+            col += cellWidth * 2;
+        }
+
+        col = cellWidth * row - cellWidth * (i + 1);
+        Console.WriteLine();
+    }
+}
+
+FillTriangle();
+Magic();
+*/
